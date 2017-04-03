b5fc724cfe18cf2b972df9e548d0823b1913f5cf
Rubem Almeida <rubem@rockes.com.br>
2017-04-03 02:04:30 -0300

    Iniciando versionamento

---
+++ Bat-Hour/Initial.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Bat_Hour
+{
+    partial class Initial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnOnOff = new System.Windows.Forms.Button();
+            this.tbxDiskUsage = new System.Windows.Forms.TextBox();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.tbxSetpoint = new System.Windows.Forms.TextBox();
+            this.cbxPort = new System.Windows.Forms.ComboBox();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOnOff
+            // 
+            this.btnOnOff.Enabled = false;
+            this.btnOnOff.Location = new System.Drawing.Point(12, 65);
+            this.btnOnOff.Name = "btnOnOff";
+            this.btnOnOff.Size = new System.Drawing.Size(192, 23);
+            this.btnOnOff.TabIndex = 0;
+            this.btnOnOff.Text = "Turn On";
+            this.btnOnOff.UseVisualStyleBackColor = true;
+            this.btnOnOff.Click += new System.EventHandler(this.btnOnOff_Click);
+            // 
+            // tbxDiskUsage
+            // 
+            this.tbxDiskUsage.Location = new System.Drawing.Point(12, 39);
+            this.tbxDiskUsage.Name = "tbxDiskUsage";
+            this.tbxDiskUsage.ReadOnly = true;
+            this.tbxDiskUsage.Size = new System.Drawing.Size(92, 20);
+            this.tbxDiskUsage.TabIndex = 1;
+            this.tbxDiskUsage.Text = "Usage: ";
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(12, 94);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(192, 173);
+            this.listBox.TabIndex = 2;
+            // 
+            // tbxSetpoint
+            // 
+            this.tbxSetpoint.Location = new System.Drawing.Point(110, 39);
+            this.tbxSetpoint.Name = "tbxSetpoint";
+            this.tbxSetpoint.Size = new System.Drawing.Size(94, 20);
+            this.tbxSetpoint.TabIndex = 3;
+            this.tbxSetpoint.Text = "Setpoint: ";
+            this.tbxSetpoint.TextChanged += new System.EventHandler(this.tbxSetpoint_TextChanged);
+            // 
+            // cbxPort
+            // 
+            this.cbxPort.FormattingEnabled = true;
+            this.cbxPort.Location = new System.Drawing.Point(110, 12);
+            this.cbxPort.Name = "cbxPort";
+            this.cbxPort.Size = new System.Drawing.Size(94, 21);
+            this.cbxPort.TabIndex = 4;
+            // 
+            // serialPort
+            // 
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(92, 21);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // Initial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(216, 279);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cbxPort);
+            this.Controls.Add(this.tbxSetpoint);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.tbxDiskUsage);
+            this.Controls.Add(this.btnOnOff);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(232, 318);
+            this.MinimumSize = new System.Drawing.Size(232, 318);
+            this.Name = "Initial";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOnOff;
+        private System.Windows.Forms.TextBox tbxDiskUsage;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.TextBox tbxSetpoint;
+        private System.Windows.Forms.ComboBox cbxPort;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Button btnConnect;
+    }
+}
+
---
+++ Bat-Hour/Initial.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bat_Hour
+{
+    public partial class Initial : Form
+    {
+
+        public PerformanceCounter myCounter = new PerformanceCounter("PhysicalDisk", "% Disk Time", "_Total");
+        Timer Timer99 = new Timer();
+        Timer timerPort = new Timer();
+        public Int32 j = 0;
+        public Int32 Setpoint = 0;
+        public bool Enabled = false;
+        public string RxString = "";
+        public string lastCommand = "";
+
+        public Initial()
+        {
+            InitializeComponent();
+
+            Timer99.Tick += Timer99_Tick; // don't freeze the ui
+            Timer99.Interval = 1024;
+            
+            timerPort.Tick += TimerPortOnTick;
+            timerPort.Interval = 500;
+            timerPort.Enabled = true;
+        }
+
+        private void TimerPortOnTick(object sender, EventArgs eventArgs)
+        {
+            atualizaListaCOMs();
+        }
+
+        private void Timer99_Tick(object sender, EventArgs eventArgs)
+        {
+            j = Convert.ToInt32(myCounter.NextValue());
+            tbxDiskUsage.Text = $"Usage: {j} %";
+            if (Enabled && j >= Setpoint)
+            {
+                if (lastCommand != "ON")
+                {
+                    serialPort.Write("ON");
+                    lastCommand = "ON";
+                }
+                listBox.Items.Add($"{DateTime.Now:hh:mm:ss} - Usage {j} %");
+            }
+            else
+            {
+                if (lastCommand != "OFF")
+                {
+                    serialPort.Write("OFF");
+                    lastCommand = "OFF";
+                }
+            }
+
+        }
+
+        private void EnableTimer()
+        {
+            Enabled = true;
+            Timer99.Enabled = true;
+            //tbxSetpoint.Text = "Enabled";
+            btnOnOff.Text = "Turn Off";
+        }
+
+        private void DisableTimer()
+        {
+            Enabled = false;
+            Timer99.Enabled = false;
+            //tbxSetpoint.Text = "Disabled";
+            btnOnOff.Text = "Turn On";
+        }
+
+        private void btnOnOff_Click(object sender, EventArgs e)
+        {
+            #region [Validação]
+            int setPoint = 0;
+            if (!int.TryParse(Regex.Match(tbxSetpoint.Text, @"\d+").Value, out setPoint))
+            {
+                MessageBox.Show(this, "Informe o valor do Setpoint.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                return;
+            }
+
+            if (!(setPoint >= 0 && setPoint <= 100))
+            {
+                MessageBox.Show(this, "Valor do Setpoint deve ser entre 1 e 100", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                return;
+            }
+
+            if (!serialPort.IsOpen)
+            {
+                MessageBox.Show(this, "Não foi possível detectar o arduino conectado.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                return;
+            }
+            #endregion
+
+            if (Enabled)
+            {
+                DisableTimer();
+            }
+            else
+            {
+                EnableTimer();
+            }
+        }
+
+        private void tbxSetpoint_TextChanged(object sender, EventArgs e)
+        {
+            string defaultText = "Setpoint: ";
+            int setPoint = 0;
+            if (!int.TryParse(Regex.Match(tbxSetpoint.Text, @"\d+").Value, out setPoint) || !(setPoint >= 0))
+            {
+                tbxSetpoint.Text = defaultText;
+                SetpointCursorEndPosition();
+                btnOnOff.Enabled = false;
+                return;
+            }
+
+            Setpoint = setPoint;
+            tbxSetpoint.Text = $"{defaultText}{setPoint}";
+            SetpointCursorEndPosition();
+            btnOnOff.Enabled = true;
+        }
+
+        private void SetpointCursorEndPosition()
+        {
+            tbxSetpoint.SelectionStart = tbxSetpoint.Text.Length;
+            tbxSetpoint.SelectionLength = 0;
+        }
+
+        private void atualizaListaCOMs()
+        {
+            int i;
+            bool quantDiferente;    //flag para sinalizar que a quantidade de portas mudou
+
+            i = 0;
+            quantDiferente = false;
+
+            //se a quantidade de portas mudou
+            if (cbxPort.Items.Count == SerialPort.GetPortNames().Length)
+            {
+                foreach (string s in SerialPort.GetPortNames())
+                {
+                    if (cbxPort.Items[i++].Equals(s) == false)
+                    {
+                        quantDiferente = true;
+                    }
+                }
+            }
+            else
+            {
+                quantDiferente = true;
+            }
+
+            //Se não foi detectado diferença
+            if (quantDiferente == false)
+            {
+                return;                     //retorna
+            }
+
+            //limpa comboBox
+            cbxPort.Items.Clear();
+
+            //adiciona todas as COM diponíveis na lista
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                cbxPort.Items.Add(s);
+            }
+            //seleciona a primeira posição da lista
+            cbxPort.SelectedIndex = 0;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen == false)
+            {
+                try
+                {
+                    serialPort.PortName = cbxPort.Items[cbxPort.SelectedIndex].ToString();
+                    serialPort.Open();
+
+                }
+                catch
+                {
+                    return;
+
+                }
+                if (serialPort.IsOpen)
+                {
+                    btnConnect.Text = "Desconectar";
+                    cbxPort.Enabled = false;
+
+                }
+            }
+            else
+            {
+
+                try
+                {
+                    serialPort.Close();
+                    cbxPort.Enabled = true;
+                    btnConnect.Text = "Conectar";
+                }
+                catch
+                {
+                    return;
+                }
+
+            }
+        }
+
+        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //RxString = serialPort.ReadLine();             //le o dado disponível na serial
+            //this.Invoke(new EventHandler(trataDadoRecebido));   //chama outra thread para escrever o dado no text box
+        }
+
+        //private void trataDadoRecebido(object sender, EventArgs e)
+        //{
+        //    Boolean ligado = 
+        //    Double tensao = Convert.ToDouble(RxString) * 5 / 1023;
+        //    lbValor.Text = "Tensão: " + Convert.ToString(Math.Round(tensao, 1)) + " V";
+        //}
+    }
+}
